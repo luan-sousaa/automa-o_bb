babe5851ddb5e0606c76ee1e81740547602f2479
luan <luanbispo.sousa263@gmail.com>
2025-04-28 00:31:15 -0300

    implementação da logica de entrar na pagina de licitaões , filtrar dados , colocar dados e por fim aplicar tudo para ver as licitaões em aberto.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
+import time
 
-#input do usuario para colocar o link da licitação
-url = input("Link do site para encontrar licitacoes :  ")
-#a variavel response guarda a request e pega a url
-response = requests.get(url)
-response.status_code
+def procurar_licitacoes():
+    with sync_playwright() as p:
+        navegador = p.chromium.launch(headless=False) #inicia nosso navegador , nesse caso o crhome
+        pagina = navegador.new_page() #abre uma nova aba no nosso navegador
+        pagina.goto("http://comprasnet.gov.br/ConsultaLicitacoes/ConsLicitacao_Filtro.asp") #vai ate nossa pagina de licitacoes
+        pagina.fill('input[name="dt_publ_ini"]', '01/04/2025') #preenche o camppo de data inicial
+        pagina.fill('input[name="dt_publ_fim"]', '14/04/2025') #preenche o camppo de data fim
+        pagina.click('input[name="ok"]')  # vai clickar no botao de ok para os filtros
+        #marca todas os filtros das licitacoes
+        pagina.click('//*[@id="frmLicitacao"]/table/tbody/tr[2]/td/table[2]/tbody/tr[4]/td[2]/table/tbody/tr/td/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table/tbody/tr[7]/td/input')
+        pagina.click('input[name="ok"]')  # vai clickar no botao de ok para os filtros
+        time.sleep(10) #vai esperar 5 segundo ate fechar meu navegador
+        # Preenchimento de filtros
+
+if __name__ == "__main__":
+    procurar_licitacoes()
